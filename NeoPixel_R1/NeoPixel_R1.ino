e6de785c5a403fa93d36c6dda8eca7726f99ffc2
nj2299 <35905887+nj2299@users.noreply.github.com>
2018-08-08 08:57:13 -0400

    test code

--- NeoPixel_R1/NeoPixel_R1.ino
+++ NeoPixel_R1/NeoPixel_R1.ino
@@ -1,7 +1,7 @@
 
 #include <FastLED.h>
 
-#define NUM_LEDS 10
+#define NUM_LEDS 5
 #define DATA_PIN D2
 
 
@@ -11,11 +11,14 @@ CRGB leds[NUM_LEDS];
 void setup() {
   // put your setup code here, to run once:
 FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+//pinMode (D2, OUTPUT);
+
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 int i=0;
 while (i<=NUM_LEDS){
     leds[i] = CRGB::Red;
@@ -31,5 +34,14 @@ while(i<=NUM_LEDS){
     delay(1000); 
     i++;
 }
+
+
+
+/*
+digitalWrite (D2,HIGH);
+delay (1000);
+digitalWrite (D2,LOW);
+delay (1000);
+*/
 
 }
